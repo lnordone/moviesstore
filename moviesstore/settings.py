5890141dc19c21d74df33866ab5e9669c320da4f
lnordonebluemvmt <lucas.nordone@bluemvmt.com>
2025-09-16 15:58:02 -0400

    fixed allowed hosts error

--- moviesstore/settings.py
+++ moviesstore/settings.py
@@ -26,7 +26,11 @@ SECRET_KEY = 'django-insecure-_kur06-$n*zu0+hw7u#)doz_3@w5k4*#zqo-aqlmb1q&0kdmkh
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'lucasnordone.pythonanywhere.com',
+    'localhost',
+    '127.0.0.1',
+]
 
 
 # Application definition
